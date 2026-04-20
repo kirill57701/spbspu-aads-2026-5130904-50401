6a808ff7f51ccb77bbcbce606606aabbf3d8eb4c
kirill <kirillpetrov56333@gmail.com>
2026-04-20 18:52:24 +0300

    adding declaration of queue

--- petrov.kirill/common/Queue.hpp
+++ petrov.kirill/common/Queue.hpp
@@ -16,6 +16,16 @@ namespace petrov {
       bool empty();
       size_t size();
       T drop();
+      Queue& operator=(const Queue& a);
+      Queue& operator=(Queue&& a);
+      const T& front() const;
+      const T& back() const;
+      bool empty() const;
+      size_t size() const;
+      void clear();
+      bool operator==(const Queue& a) const;
+      bool operator!=(const Queue& a) const;
+      void swap(Queue& a) noexcept;
     private:
       petrov::List<T> dat;
   }
