971831618d1b266b4e50f072b78d6d2fcc9e2b0c
kirill <kirillpetrov56333@gmail.com>
2026-04-20 20:18:58 +0300

    stack

--- petrov.kirill/common/Stack.hpp
+++ petrov.kirill/common/Stack.hpp
@@ -8,10 +8,34 @@ namespace petrov
   class Stack
   {
     public:
+      Stack() = default;
+      Stack(Stack&& a) = default;
+      Stack(const Stack& a) = default;
+      ~Stack();
+
       void push(T rhs);
+      void push(T&& rhs);
+      void pop();
       T drop();
+
+      T top();
+      const T& top() const;
+
+      bool empty() const;
+      size_t size() const;
+      void clear();
+
+      Stack& operator=(const Stack& a);
+      Stack& operator=(Stack&& a);
+      bool operator==(const Stack& a) const;
+      bool operator!=(const Stack& a) const;
+
+    private:
+      petrov::List<T> dat;
   };
 }
+
+
 
 #endif
 
