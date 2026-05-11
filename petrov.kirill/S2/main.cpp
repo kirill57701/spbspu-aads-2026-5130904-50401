09c77739c467da8ae79053a9495839ec5abcae96
Kirill <kirillpetrov56333@gmail.com>
2026-05-12 01:33:59 +0300

    main

--- petrov.kirill/S2/main.cpp
+++ petrov.kirill/S2/main.cpp
@@ -1,6 +1,36 @@
 #include <iostream>
+#include <fstream>
+#include <exception>
+#include "utils.hpp"
+#include "Stack.hpp"
 
-int main()
+int main(int argc, char* argv[])
 {
-  std::cout << "petrov.kirill/S2";
+  try
+  {
+    std::ifstream ifs;
+    if (argc > 1)
+    {
+      file.open(argv[1]);
+      if (!ifs.is_open())
+      {
+        std::cerr << "err\n";
+        return 1;
+      }
+    }
+    std::istream& in = (argc > 1) ? ifs : std::cin;
+    petrov::Stack<long long int> results = petrov::calcStream(in);
+
+    while (!results.empty())
+    {
+      std::cout << results.drop() << (results.empty() ? "" : " ");
+    }
+    std::cout << "\n";
+  }
+  catch (const std::exception& e)
+  {
+    std::cerr << e.what() << "\n";
+    return 1;
+  }
+  return 0;
 }
