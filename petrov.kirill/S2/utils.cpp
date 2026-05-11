d4a9e6cf8dc6b67d206e010c4b6fa2736bf99b49
Kirill <kirillpetrov56333@gmail.com>
2026-05-11 22:07:51 +0300

    come utils

---
+++ petrov.kirill/S2/utils.cpp
@@ -0,0 +1,51 @@
+#include "utils.hpp"
+#include "Stack.hpp"
+#include "Queue.hpp"
+#include <limits>
+#include <stdexcept>
+
+namespace petrov
+{
+  size_t prior(char s)
+  {
+    if (s == '+' || s == '-')
+    {
+      return 2;
+    }
+    else if (s == '*' || s == '/' || s == '%')
+    {
+      return 3;
+    }
+    return 1;
+  }
+
+  long long int oper(long long int a, long long int b, char s)
+  {
+    if (s != '/' && s != '%')
+    {
+      return (s == '+' ? a + b : (s == '-' ? a - b : a*b);
+    }
+    if (s == '/')
+    {
+      if (b == 0)
+      {
+	throw std::logic_error("err\n");
+      }
+      return a/b;
+    }
+    if (s == '%')
+    {
+      if (b == 0)
+      {
+        throw std::logic_error("err\n");
+      }
+      return a%b;
+    }
+    throw std::logic_error("err\n");
+  }
+
+  size_t isOp(char s)
+  {
+    return s == '+' || s == '-' || s == '*' || s == '%' || s == '/';
+  }
+}
