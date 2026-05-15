e3717972fa70653c3a37cb12fb69d63c337f48b2
Neobo1 <kp1682674@gmail.com>
2026-05-15 13:59:54 +0000

    declarators hashtable

--- petrov.kirill/common/HashTable.hpp
+++ petrov.kirill/common/HashTable.hpp
@@ -1,17 +1,30 @@
 #ifndef HASHTABLE_HPP
 #define HASHTABLE_HPP
+#include <cstddef>
+#include "list.hpp"
 
 namespace petrov
 {
-  template<class Key, class Value, class Equal>
+  template<class Key, class Value, class Hash, class Equal>
   class HashTable
   {
-    public:
-      void add (Key k, Value v);
-      Value drop (Key k);
-      bool has (Kay k);
-      void rehash (size_t slots);
-  };
+    private:
+      struct Entry {
+        Key key;
+        Value value;
+      };
 
+    Entry* table;
+    size_t size;
+    size_t capacity;
+    public:
+      HashTable();
+      ~HashTable();
+      void add(Key k, Value v);
+      Value drop(Key k);
+      bool has(Key k);
+      void rehash(size_t slots);
+  };
 }
+
 #endif
