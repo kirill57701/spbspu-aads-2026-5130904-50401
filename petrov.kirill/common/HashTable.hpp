d87360bfa9e6fccab26c6f1af0df7a2609ba8020
Kirill <kirillpetrov56333@gmail.com>
2026-06-08 02:04:39 +0300

    hash

--- petrov.kirill/common/HashTable.hpp
+++ petrov.kirill/common/HashTable.hpp
@@ -3,28 +3,25 @@
 #include <cstddef>
 #include "list.hpp"
 
-namespace petrov
-{
-  template<class Key, class Value, class Hash, class Equal>
-  class HashTable
-  {
-    private:
-      struct Entry {
-        Key key;
-        Value value;
-      };
+namespace petrov {
+  template <class Key, class Value, class HashFunc = Hash<Key>, class Equal = std::equal_to<Key>>
+  class HashTable {
+  private:
+    struct Node {
+      std::pair<Key, Value> kv;
+      Node *next;
+      Node(const Key &k, const Value &v, Node *n) : kv(k, v), next(n) {}
+    };
 
-    Entry* table;
-    size_t size;
-    size_t capacity;
-    public:
-      HashTable();
-      ~HashTable();
-      void add(Key k, Value v);
-      Value drop(Key k);
-      bool has(Key k);
-      void rehash(size_t slots);
+    Node **buckets_;
+    size_t bucketCount_;
+    size_t size_;
+    HashFunc hasher_;
+    Equal equal_;
+  public:
+    HashTable() : buckets_(new Node*[16]()), bucketCount_(16), size_(0) {}
   };
 }
+
 
 #endif
