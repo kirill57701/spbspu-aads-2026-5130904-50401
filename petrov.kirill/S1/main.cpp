15e9b4726adca9eef5798e96f94ae80e56922756
Kirill <kirillpetrov56333@gmail.com>
2026-06-08 15:11:25 +0300

    fix

--- petrov.kirill/S1/main.cpp
+++ petrov.kirill/S1/main.cpp
@@ -6,12 +6,12 @@
 
 int main()
 {
-  petrov::List<std::pair<std::string, petrov::List<unsigned long long>>> list_for_sol;
+  petrov::List<std::pair<std::string, petrov::List< size_t >>> list_for_sol;
   std::string name;
 
   while (std::cin >> name)
   {
-    petrov::List<unsigned long long> count_nums;
+    petrov::List< size_t > count_nums;
     unsigned long long t = 0;
 
     while (std::cin >> t)
@@ -35,7 +35,7 @@ int main()
     return 0;
   }
 
-  petrov::LIter<std::pair<std::string, petrov::List<unsigned long long>>> q_names = list_for_sol.begin();
+  petrov::LIter<std::pair<std::string, petrov::List< size_t >>> q_names = list_for_sol.begin();
   while (q_names != list_for_sol.end())
   {
     std::cout << q_names->first;
@@ -49,7 +49,7 @@ int main()
   std::cout << '\n';
 
   bool all_empty = 1;
-  petrov::LIter<std::pair<std::string, petrov::List<unsigned long long>>> check_empty = list_for_sol.begin();
+  petrov::LIter<std::pair<std::string, petrov::List< size_t >>> check_empty = list_for_sol.begin();
   while (check_empty != list_for_sol.end())
   {
     if (!check_empty->second.empty())
@@ -66,23 +66,23 @@ int main()
     return 0;
   }
 
-  petrov::List<petrov::LIter<unsigned long long>> iters;
-  petrov::LIter<std::pair<std::string, petrov::List<unsigned long long>>> q_init = list_for_sol.begin();
+  petrov::List<petrov::LIter< size_t >> iters;
+  petrov::LIter<std::pair<std::string, petrov::List< size_t >>> q_init = list_for_sol.begin();
   while (q_init != list_for_sol.end())
   {
     iters.push_back(q_init->second.begin());
     ++q_init;
   }
 
-  petrov::List<unsigned long long> sums;
+  petrov::List< size_t > sums;
   bool isleft = 1;
   while (isleft)
   {
     unsigned long long current_row_sum = 0;
     bool first_in_row = 1;
     isleft = 0;
-    petrov::LIter<petrov::LIter<unsigned long long>> its_it = iters.begin();
-    petrov::LIter<std::pair<std::string, petrov::List<unsigned long long>>> data_it = list_for_sol.begin();
+    petrov::LIter<petrov::LIter< size_t >> its_it = iters.begin();
+    petrov::LIter<std::pair<std::string, petrov::List< size_t >>> data_it = list_for_sol.begin();
 
     while (its_it != iters.end())
     {
@@ -98,7 +98,7 @@ int main()
         std::cout << val;
         first_in_row = 0;
 
-        if (std::numeric_limits<unsigned long long>::max() - current_row_sum < val)
+        if (std::numeric_limits< size_t >::max() - current_row_sum < val)
         {
           std::cout << '\n';
           std::cerr << "overflow\n";
@@ -120,7 +120,7 @@ int main()
     sums.push_back(current_row_sum);
   }
 
-  petrov::LIter<unsigned long long> s_it = sums.begin();
+  petrov::LIter< size_t > s_it = sums.begin();
   while (s_it != sums.end())
   {
     std::cout << *s_it;
