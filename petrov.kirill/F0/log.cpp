5f7ab85aa467032d2a890fb6234b8612ef211361
Kirill <kirillpetrov56333@gmail.com>
2026-06-17 16:13:15 +0300

    test fitting logistic

--- petrov.kirill/F0/log.cpp
+++ petrov.kirill/F0/log.cpp
@@ -8,8 +8,14 @@ int main()
   model.load_model(std::cin);
   model.model_info(std::cout);
   petrov::List<double> inp;
-  inp.push_back(1);
   inp.push_back(2);
+  inp.push_back(3);
   double pred = model.model_predict(inp);
-  std::cout << pred;
+  std::cout << pred << '\n';
+  double g = 1, a = 0.5;
+  for (size_t k = 0; k < 10; ++k)
+  {
+    model.model_fit(inp, g, pred, a);
+  }
+  std::cout << model.model_predict(inp) << '\n';
 }
